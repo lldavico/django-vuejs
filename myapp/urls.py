8886b3959d6de189d4606a5c13411e07d5177687
Luciano <lldavico@uc.cl>
2020-07-10 23:34:32 -0400

    init vue frontend

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import HomeAPIView, ArticleAPIView, ArticleDetails
+# from .views import article_list, article_detail
 
+
+urlpatterns = [
+    path('articles/', ArticleAPIView.as_view()),
+    path('articles/<int:id>/', ArticleDetails.as_view()),
+    path('', HomeAPIView.as_view())
+]
+
+'''
 urlpatterns = [
     path('articles/', article_list),
     path('articles/<int:pk>/', article_detail)
 ]
+'''
